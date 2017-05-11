8846dbeb0a0e1b3e6690d75ef8aa2094e1d176ab
Ryan French <ryan.lloyd.french@gmail.com>
2017-05-11 15:31:37 +1000

    Improvements to gulpfile.

--- gulpfile.js
+++ gulpfile.js
@@ -1,36 +1,79 @@
-var gulp = require('gulp');
-var sass = require('gulp-sass');
-var browserSync = require('browser-sync').create();
-var imagemin = require('gulp-imagemin');
-var cache = require('gulp-cache');
-var notify = require("gulp-notify");
+const gulp = require('gulp');
+const htmlmin = require('gulp-htmlmin');
+const sass = require('gulp-sass');
+const sourcemaps = require('gulp-sourcemaps');
+const autoprefixer = require('gulp-autoprefixer');
+const cssnano = require('gulp-cssnano');
+const jshint = require('gulp-jshint');
+const uglify = require('gulp-uglify');
+const rename = require('gulp-rename');
+const concat = require('gulp-concat');
+const browsersync = require('browser-sync').create();
+const imagemin = require('gulp-imagemin');
+const newer = require('gulp-newer');
+const clean = require('gulp-clean');
+const runsequence = require('run-sequence');
+const notify = require("gulp-notify");
 
-gulp.task('sass', function() {
+gulp.task('css', function() {
     return gulp.src('src/scss/**/*.scss')
+        .pipe(sourcemaps.init())
         .pipe(sass())
-        .pipe(gulp.dest('src/css'))
-        .pipe(browserSync.reload({
-            stream: true
-        }))
-        .pipe(notify({message: 'Compiled SCSS'}))
+        .pipe(autoprefixer({browsers: ['last 2 versions'],cascade: false}))
+        .pipe(cssnano())
+        .pipe(rename({suffix: '.min'}))
+        .pipe(sourcemaps.write('sourcemaps'))
+        .pipe(gulp.dest('dist/css'))
+        .pipe(browsersync.reload({stream: true}))
+        .pipe(notify({message: 'Compiled CSS'}))
 });
 
-gulp.task('browserSync', function() {
-    browserSync.init({
+gulp.task('html', function() {
+    return gulp.src('src/**/*.html')
+        .pipe(newer('dist'))
+        .pipe(htmlmin({removeComments: true,collapseWhitespace: true}))
+        .pipe(gulp.dest('dist'))
+        .pipe(browsersync.reload({stream: true}))
+        .pipe(notify({message: 'Compiled HTML'}))
+});
+
+gulp.task('js', function() {
+    return gulp.src('src/js/**/*.js')
+        .pipe(jshint())
+        .pipe(jshint.reporter('jshint-stylish'))
+        .pipe(concat('scripts.js'))
+        .pipe(rename({suffix: '.min'}))
+        .pipe(uglify())
+        .pipe(gulp.dest('dist/js'))
+        .pipe(notify({message: 'Compiled JS'}))
+});
+
+gulp.task('browsersync', function() {
+    browsersync.init({
         server: {
-            baseDir: 'src'
+            baseDir: 'dist'
         },
+        open: false
     })
 })
 
 gulp.task('images', function() {
     return gulp.src('src/images/**/*.+(png|jpg|gif|svg)')
-        .pipe(cache(imagemin()))
+        .pipe(newer('dist/images'))
+        .pipe(imagemin({optimizationLevel: 7, progressive: true, interlaced: true}))
         .pipe(gulp.dest('dist/images'))
 })
 
-gulp.task('watch', ['browserSync', 'sass'], function(){
-    gulp.watch('src/scss/**/*.scss', ['sass']);
-    gulp.watch('src/**/*.html', browserSync.reload);
-    gulp.watch('src/js/**/*.js', browserSync.reload);
+gulp.task('clean', function() {
+    return gulp.src('dist')
+        .pipe(clean())
 })
+
+gulp.task('watch', function(callback) {
+    runsequence('clean', 'html', 'css', 'js', 'images', 'browsersync', callback);
+    gulp.watch('src/scss/**/*.scss', ['css']);
+    gulp.watch('src/**/*.html', ['html']);
+    gulp.watch('src/js/**/*.js', ['js']);
+});
+
+gulp.task('default', ['watch']);
