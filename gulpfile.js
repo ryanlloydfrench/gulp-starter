a7cb0d380901c8866a0e43cc0194709a51f60279
Ryan French <ryan.lloyd.french@gmail.com>
2017-07-31 15:11:18 +1000

    Add variable paths.

--- gulpfile.js
+++ gulpfile.js
@@ -14,68 +14,72 @@ const clean = require('gulp-clean');
 const runsequence = require('run-sequence');
 const notify = require("gulp-notify");
 
+const path = {
+	src: 'src/',
+	dist: 'dist/'
+};
+
 gulp.task('css', function() {
-    return gulp.src('src/scss/**/*.scss')
+    return gulp.src(path.src + 'scss/**/*.scss')
         .pipe(sourcemaps.init())
         .pipe(sass())
         .pipe(autoprefixer({browsers: ['last 2 versions'],cascade: false}))
         .pipe(cssnano())
         .pipe(rename({suffix: '.min'}))
         .pipe(sourcemaps.write('sourcemaps'))
-        .pipe(gulp.dest('dist/css'))
+        .pipe(gulp.dest(path.dist + 'css'))
         .pipe(browsersync.reload({stream: true}))
         .pipe(notify({message: 'Compiled CSS', onLast: 'true'}))
 });
 
 gulp.task('html', function() {
-    return gulp.src('src/**/*.html')
-        .pipe(newer('dist'))
+    return gulp.src(path.src + '**/*.html')
         .pipe(htmlmin({removeComments: true,collapseWhitespace: true}))
-        .pipe(gulp.dest('dist'))
+        .pipe(gulp.dest(path.dist))
         .pipe(browsersync.reload({stream: true}))
         .pipe(notify({message: 'Compiled HTML', onLast: 'true'}))
 });
 
 gulp.task('js', function() {
-    return gulp.src('src/js/**/*.js')
+    return gulp.src(path.src + 'js/**/*.js')
         .pipe(concat('scripts.js'))
         .pipe(rename({suffix: '.min'}))
         .pipe(uglify())
-        .pipe(gulp.dest('dist/js'))
+        .pipe(gulp.dest(path.dist + 'js'))
         .pipe(notify({message: 'Compiled JS', onLast: 'true'}))
 });
 
 gulp.task('browsersync', function() {
     browsersync.init({
         server: {
-            baseDir: 'dist'
+            baseDir: path.dist
         },
         open: false
     })
 })
 
 gulp.task('images', function() {
-    return gulp.src('src/images/**/*.+(png|jpg|gif|svg)')
+    return gulp.src(path.src + 'images/**/*.+(png|jpg|gif|svg)')
         .pipe(imagemin([
             imagemin.gifsicle({interlaced: true}),
             imagemin.jpegtran({progressive: true}),
             imagemin.optipng({optimizationLevel: 3}),
             imagemin.svgo({plugins: [{removeViewBox: true}]})
         ]))
-        .pipe(gulp.dest('dist/images'))
+        .pipe(gulp.dest(path.dist + 'images'))
         .pipe(notify({message: 'Compiled Images', onLast: 'true'}))
 })
 
 gulp.task('clean', function() {
-    return gulp.src('dist')
+    return gulp.src(path.dist)
         .pipe(clean())
 })
 
 gulp.task('watch', function(callback) {
     runsequence('clean', 'html', 'css', 'js', 'images', 'browsersync', callback);
-    gulp.watch('src/scss/**/*.scss', ['css']);
-    gulp.watch('src/**/*.html', ['html']);
-    gulp.watch('src/js/**/*.js', ['js']);
+    gulp.watch(path.src + 'scss/**/*.scss', ['css']);
+    gulp.watch(path.src + '**/*.html', ['html']);
+    gulp.watch(path.src + 'js/**/*.js', ['js']);
 });
 
 gulp.task('default', ['watch']);
